8fa5a2c786de3471af995dab42f197da4894d088
István Antal <istvan.m.antal@gmail.com>
2016-08-30 06:34:45 +0100

    Set the author to current user

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -46,6 +46,7 @@ class PostController extends Controller {
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+            $post->setUser($this->getUser());
             $em->persist($post);
             $em->flush();
 
